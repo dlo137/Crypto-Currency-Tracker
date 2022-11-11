4886a52e2c780f34f80acf387c8362708bbf35dc
dlo137 <dangelo.watson1212@gmail.com>
2022-11-11 14:23:46 -0500

    Installed Axios

--- src/App.js
+++ src/App.js
@@ -19,7 +19,10 @@ useEffect(() => {
   return (
     <div className="coin-app">
       <div className= 'coin-search'>
-        <h1 className= 'coin-text'>search a currency</h1>
+        <h1 className= 'coin-text'>Search a currency</h1>
+        <form>
+          <input type='text' placeholder= 'Search' className='coin-input'/>
+        </form>
       </div>
     </div>
   );
